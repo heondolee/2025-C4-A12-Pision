a3ad170f977a324df997f869863cd6e595317ecc
YeoSeongil <seongil_yeo@naver.com>
2025-07-14 20:17:41 +0900

    [Setting] 프로젝트 생성

---
+++ Pision/Pision/PisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PisionApp.swift
+//  Pision
+//
+//  Created by 여성일 on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PisionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
