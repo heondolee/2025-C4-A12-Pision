0b45086789cf252008f78e234e4f59d11afbf9a8
rundo <rundo999@gmial.com>
2025-07-15 15:48:48 +0900

    [FEAT] CriteriaSettingView UI 그리기

--- Pision/Pision/Source/Feature/CriteriaSetting/CriteriaSettingView.swift
+++ Pision/Pision/Source/Feature/CriteriaSetting/CriteriaSettingView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct CriteriaSettingView: View {
+  @State private var isSleepyOn = false
+  @State private var isEyeTrackingOn = false
+  @State private var isPostureOn = false
+  @State private var isGazeOn = false
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack() { // 뷰 전체 싸기
+      // 토글 항목 박스
+      VStack(spacing: 12) {
+        Toggle("졸음 측정", isOn: $isSleepyOn)
+        Divider()
+        Toggle("눈동자 측정", isOn: $isEyeTrackingOn)
+        Divider()
+        Toggle("자세 측정", isOn: $isPostureOn)
+        Divider()
+        Toggle("시선 측정", isOn: $isGazeOn)
+      }
+      .padding(16)
+      .background(Color(.systemGray5))
+      .cornerRadius(12)
+      
+      Spacer()
+      
+      Button(action: {
+        print("커스텀 수정 저장하기")
+      }) {
+        Text("저장")
+          .frame(maxWidth: .infinity)
+          .padding(12)
+          .background(Color(.systemGray4))
+          .cornerRadius(12)
+      }
+    }
+    .padding(12)
   }
 }
 
