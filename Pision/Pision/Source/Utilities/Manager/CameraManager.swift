f824144b22ad9393bda6aad71af906782fca1837
YeoSeongil <seongil_yeo@naver.com>
2025-07-21 23:51:30 +0900

    [FEAT] 카메라 매니저 구현

--- Pision/Pision/Source/Utilities/Manager/CameraManager.swift
+++ Pision/Pision/Source/Utilities/Manager/CameraManager.swift
@@ -1,48 +1,24 @@
 //
 //  CameraManager.swift
-//  PisionTest1
+//  Pision
 //
 //  Created by 여성일 on 7/9/25.
 //
 
 import AVFoundation
-import SwiftUI
-import Vision
 
-final class CameraManager: NSObject, ObservableObject {
+final class CameraManager: NSObject {
   let session = AVCaptureSession()
   
   private let videoOutput = AVCaptureVideoDataOutput()
-  private var isSeesionConfigured = false
+  private var isSessionConfigured = false
   private let sessionQueue = DispatchQueue(label: "CameraSessionQueue")
   
-  private let visionManager = VisionManager()
+  private let visionManager: VisionManager
   
-  /*
-   Vision 처리 결과를 외부로 넘겨주기 위한 클로저
-   Vision 처리 값을 외부 객체나 ViewModel에서 사용할 수 있도록 전달합니다
-   */
-  var onYawsUpdate: (([Double]) -> Void)?
-  var onRollsUpdate: (([Double]) -> Void)?
-  var onPoseUpdate: ((VNHumanBodyPoseObservation) -> Void)?
-  
-  override init() {
+  init(visionManager: VisionManager) {
+    self.visionManager = visionManager
     super.init()
-    
-    // 비전매니저에서 비전 분석 결과를 클로저로 받아와서 처리 결과를 클로저로 외부(여기서는 뷰모델에 전달할거임)에 전달
-    // 각 클로저는 ViewModel에서 처리함
-    visionManager.onFaceDetection = { [weak self] _, yaws, rolls in
-      DispatchQueue.main.async {
-        self?.onYawsUpdate?(yaws)
-        self?.onRollsUpdate?(rolls)
-      }
-    }
-    
-    visionManager.onPoseDetection = { [weak self] observation in
-      DispatchQueue.main.async {
-        self?.onPoseUpdate?(observation)
-      }
-    }
   }
   
   func startSession() {
@@ -68,34 +44,37 @@ final class CameraManager: NSObject, ObservableObject {
         if granted {
           self?.configureSessionIfNeeded()
         } else {
-          print("사용자가 카메라 접근을 거부했습니다.")
+          print("카메라 접근 거부됨")
         }
       }
       
     case .authorized:
       configureSessionIfNeeded()
-      
     case .restricted, .denied:
-      print("카메라 접근이 제한되었거나 거부됨")
-      
+      print("카메라 접근 제한 또는 거부")
     @unknown default:
       print("알 수 없는 권한 상태")
     }
   }
-}
-
-extension CameraManager {
+  
+  private func configureSessionIfNeeded() {
+    guard !isSessionConfigured else { return }
+    isSessionConfigured = true
+    configureSession()
+  }
+  
   private func configureSession() {
     session.beginConfiguration()
     session.sessionPreset = .high
     
-    guard let device = AVCaptureDevice.default(.builtInWideAngleCamera ,for: .video, position: .front),
+    guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front),
           let input = try? AVCaptureDeviceInput(device: device),
           session.canAddInput(input) else {
-      print("Log: 카메라 인풋 설정 실패")
+      print("카메라 인풋 실패")
       session.commitConfiguration()
       return
     }
+    
     session.addInput(input)
     
     if session.canAddOutput(videoOutput) {
@@ -111,20 +90,9 @@ extension CameraManager {
       } else {
         videoOutput.connections.first?.videoOrientation = .portrait
       }
-      
-      session.commitConfiguration()
     }
     
     session.commitConfiguration()
-  }
-  
-  private func configureSessionIfNeeded() {
-    guard !isSeesionConfigured else {
-      print("이미 세션이 구성되어 있음")
-      return
-    }
-    isSeesionConfigured = true
-    configureSession()
   }
 }
 
